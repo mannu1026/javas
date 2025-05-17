e32bf6552a2745e5df0944239787775ad7adc049
mannu1026 <arvindsingh456786@gmail.com>
2025-05-17 23:03:36 +0530

    comparison of datatypes

---
+++ lecture8.js
@@ -0,0 +1,31 @@
+// conparison of datatypes in JS
+
+// console.log(2<21)
+// console.log(2>21)
+// console.log(2<=21)
+// console.log(2>=2)
+// console.log(2 == 2)
+
+// in JS allow us to compare different datatypes (as converting string to number)such as : -----------------------
+
+// console.log("3">2)
+// console.log("3"<2)
+
+
+// comparison of null value--------------------------------------
+
+// console.log(null < 0); => False
+// console.log(null == 0); => False
+// console.log(null > 0); => False
+// console.log(null >= 0); => true
+// console.log(null <= 0); => true
+
+// but "undefined" gives ALWAYS FALSE VALUES TPO YOU 
+
+
+
+// STRICT CHECK (CHECKING BY TRIPLE EQUAL OPERATOR ===); here it check datatype too. -------------------------
+   console.log("3"===3);
+
+
+
