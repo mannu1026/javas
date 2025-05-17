e0ae809f4632301132708e6c8a3b3e60e1ce0824
mannu1026 <arvindsingh456786@gmail.com>
2025-05-17 22:36:17 +0530

    number-string conversion

--- lecture7.js
+++ lecture7.js
@@ -1,3 +1,31 @@
-// why string to number conversion is so cobnfusing
+// why string to number conversion is so cobnfusing ------------------
 
-console.log(5)
+// let value = 3;
+// let negative = -value;
+
+// console.log(negative);
+
+
+
+// let str1 = "hello";
+// let str2 = "maninder";
+
+// let str3 = str1 + " " + str2;
+// console.log(str3)
+
+//  addition of number and string together -----------------------------------------
+
+// console.log("1" + 3);
+// console.log(2 + "3");
+// console.log("1" + 3 + 4);  
+       // // here string is at frist thats why whole calculation behave as string 
+// console.log(3 + 3 + "3");  
+       // //  but here numerical value is at first that's why numbers behave as number until first string typr value is not occured
+// console.log(3 + 3 + "3" + 4);
+ 
+//  if you want to add numbers in operation where string occurs at first . 
+// then, do you paranthesis to prioritize the operations.
+
+
+
+
